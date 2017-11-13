fdc2c25372843f3122007b24e3d664bd3635e8f5
Michael Sollami <msollami@shutterstock.com>
2017-11-13 17:38:54 -0500

    Tweaks

--- install.sh
+++ install.sh
@@ -2,14 +2,16 @@
 cwd=$PWD
 cd $HOME
 
+echo "Removing old dotfiles"
 rm -f .zshrc 
 rm -f .zsh_aliases
 rm -f .zsh_functions
 rm -f .vimrc
 rm -f .tmux.conf
 rm -f .gitignore
-rm $HOME/.ipython/profile_default/ipython_config.py 
+rm -f $HOME/.ipython/profile_default/ipython_config.py 
 
+echo "Installing new dotfiles"
 ln -s $cwd/.zshrc .zshrc
 ln -s $cwd/.zsh_aliases .zsh_aliases
 ln -s $cwd/.zsh_functions .zsh_functions
